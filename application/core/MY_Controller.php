e83b15d7949f1ec190fefaa8ff7e38f45f3925fe
xiaoyaohc <295698932@qq.com>
2017-02-13 14:32:40 +0800

    项目部署-完成后台首页

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -18,4 +18,3 @@ class Admin_Controller extends CI_Controller {
         $this->load->switch_themes_off();
     }
 }
-//定义后台总控制器
--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -4,6 +4,7 @@ class MY_Loader extends CI_Loader {
     protected $_theme='default/';//默认模板
     #开始皮肤功能
     public function switch_themes_on(){
+
         $this->_ci_view_paths = array(THEMES_DIR.$this->_theme=> TRUE);
     }
     #关闭皮肤功能
